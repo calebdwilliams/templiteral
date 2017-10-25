5b3a933539546f0c5fb998419fe331f97ecbb301
Caleb Williams <caleb.williams@capitalone.com>
2017-10-25 15:39:31 -0500

    build(rollup): Moved build to rollup

--- utils.js
+++
@@ -1,6 +0,0 @@
-/* eslint-env node */
-const isProd = (isTrue, isFalse = () => {}) => process.env.NODE_ENV === 'production' ? isTrue : isFalse;
-
-const noop = () => {};
-
-module.exports = { isProd, noop };
