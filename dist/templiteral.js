d184288face5707bdd49c9815ea6f1d34cc972f5
Caleb Williams <caleb.d.williams@gmail.com>
2018-01-11 12:00:31 -0600

    fix(templateCache): Changed templateCache to a WeakMap

--- dist/templiteral.js
+++ dist/templiteral.js
@@ -207,20 +207,19 @@ class Template {
   }
 }
 
-const templateCache = new Map();
+const templateCache = new WeakMap();
 
 function templiteral(location = this, context = this) {
   location.shadowRoot ? location = location.shadowRoot : null;
 
   return (strings, ...values) => {
-    const templateKey = (strings.join(''));
-    let compiler = templateCache.get(templateKey);
+    let compiler = templateCache.get(location);
 
     if (compiler) {
       compiler.update(values);
     } else {
       compiler = new Template(strings, values, location, context);
-      templateCache.set(templateKey, compiler);
+      templateCache.set(location, compiler);
     }
 
     return compiler;
--- src/templiteral.js
+++ src/templiteral.js
@@ -1,19 +1,18 @@
 import { Template } from './Template.js';
 
-const templateCache = new Map();
+const templateCache = new WeakMap();
 
 export function templiteral(location = this, context = this) {
   location.shadowRoot ? location = location.shadowRoot : null;
 
   return (strings, ...values) => {
-    const templateKey = (strings.join(''));
-    let compiler = templateCache.get(templateKey);
+    let compiler = templateCache.get(location);
 
     if (compiler) {
       compiler.update(values);
     } else {
       compiler = new Template(strings, values, location, context);
-      templateCache.set(templateKey, compiler);
+      templateCache.set(location, compiler);
     }
 
     return compiler;
